8b1bd59e0a011e9233fba452a29690f792a48331
mperleo <tiomakelelle@outlook.com>
2021-11-08 11:24:03 +0100

    funciones de perfil de usuario codificadas pero sin probar

--- src/main/java/curso/java/tienda/controllers/UsuarioGest.java
+++ src/main/java/curso/java/tienda/controllers/UsuarioGest.java
@@ -29,17 +29,57 @@ public class UsuarioGest {
 		return "ver";
 	}
 	
-	@PostMapping("modificar")
-	public String ver(@ModelAttribute Usuario usuario, HttpSession session, Model model) {
-		Usuario  user = (Usuario) session.getAttribute("usuario");
-		usuario.setClave(user.getClave());
-		return "ver";
+	@GetMapping("modificar/")
+	public String modificarUsuario(HttpSession session) {
+		return "modificar";
 	}
 	
-	@PostMapping("modificarContraseña")
-	public String modificarContraseña(HttpSession session, Model model, @RequestParam String contraComp, @RequestParam String contraNueva, @RequestParam String contraAct) {
+	@PostMapping("modificar/guardar")
+	public String modificarUsuarioGuardar(@ModelAttribute Usuario usuario, HttpSession session, Model model) {
+		Usuario  user = (Usuario) session.getAttribute("usuario");
+		// meto los datos que del usuario que no estan en el formulario
+		usuario.setId(user.getId());
+		usuario.setClave(user.getClave());
+		
+		// compruebo si el correo indicado es unico en la bbdd o si no lo ha cambiado al guardar
+		Usuario compEmail = us.getUsuarioXEmail(usuario.getEmail());
+		if(compEmail == null || compEmail.getId() == usuario.getId()) {
+			us.editUsuario(usuario);
+			model.addAttribute("mensajeOk", "Datos de usuario modficados correctamente");
+			return "ver";
+		}
+		else {
+			model.addAttribute("mensaje", "Has indicado un correo no valido");
+			return "modificar";
+		}
+	}
+	
+	@GetMapping("modificarPass")
+	public String modificarContraseña(HttpSession session) {
+		return "modificarPass";
+	}
+	
+	@PostMapping("modificarPass/guardar")
+	public String modificarContraseñaGuardar(HttpSession session, Model model, @RequestParam String contraComp, @RequestParam String contraNueva, @RequestParam String contraAct) {
 		Usuario user = (Usuario) session.getAttribute("usuario");
-		user.setClave(contraNueva);
-		return "ver";
+		// meto los datos que del usuario que no estan en el formulario
+		if(user.getClave().equals(contraAct)) {
+			if(contraComp.equals(contraNueva)) {
+				user.setClave(contraNueva);
+				us.editUsuario(user);
+				
+				model.addAttribute("mensajeOk", "Contraseña modficada correctamente");
+				return "ver";
+			}
+			else {
+				model.addAttribute("mensaje", "Las contraseñas indicadas no son iguales");
+				return "modificarPass";
+			}
+		}
+		else {
+			model.addAttribute("mensaje", "Tienes que indicar tu contraseña actual");
+			return "modificarPass";
+		}
+
 	}
 }
