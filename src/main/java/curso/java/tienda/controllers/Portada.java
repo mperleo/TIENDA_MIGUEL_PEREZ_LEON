62808138022c5407a766d790a1da0bc82735640e
mperleo <tiomakelelle@outlook.com>
2021-11-15 00:47:52 +0100

    añadido controlador y vista para los enlaces del footer a cerca de la página

--- src/main/java/curso/java/tienda/controllers/Portada.java
+++ src/main/java/curso/java/tienda/controllers/Portada.java
@@ -7,12 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import curso.java.tienda.models.entities.Categoria;
 import curso.java.tienda.models.entities.Descuento;
 import curso.java.tienda.models.entities.Producto;
-import curso.java.tienda.service.CategoriaService;
 import curso.java.tienda.service.DescuentoService;
 import curso.java.tienda.service.ProductoService;
 import curso.java.tienda.service.UsuarioService;
@@ -24,8 +21,6 @@ public class Portada {
 	
 	@Autowired
 	private ProductoService ps;
-	@Autowired
-	private CategoriaService cs;
 	@Autowired
 	private UsuarioService us;
 	@Autowired
@@ -59,23 +54,8 @@ public class Portada {
 		return "index";
 	}
 	
-	@GetMapping("/tienda")
-	public String verCat(Model model, @RequestParam(required=false, defaultValue="todos") String id_cat, @RequestParam(required=false, defaultValue="1") String orden) {
-		List<Producto> productos = null;
-		
-		List<Categoria> cats = cs.getListaCategorias();
-		
-		productos = ps.getListaProductosFiltro(id_cat, Integer.parseInt(orden));
-		
-		if(!id_cat.equals("todos")) {
-			Categoria categoria = cs.getCategoriaXId(Integer.parseInt(id_cat));
-			model.addAttribute("categoria", categoria);
-		}
-		
-		model.addAttribute("productos", productos);
-		model.addAttribute("categorias", cats);
-		model.addAttribute("nRes", productos.size());
-		
-		return "shop";
+	@GetMapping("/about")
+	public String aCercaDe() {
+		return "about";
 	}
 }
