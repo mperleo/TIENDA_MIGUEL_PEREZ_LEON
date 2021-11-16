03e6b2d41a7c26b806f167fc4de4551af2bf00ed
mperleo <tiomakelelle@outlook.com>
2021-11-16 10:48:24 +0100

    javadoc de la clase login y del filtro generado y modificado con codificación utf-8 para que tildes y ñ salgran correctamente

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"curso.java.tienda.controllers"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"curso.java.tienda.Filters"}];updateSearchResults();
--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"curso.java.tienda.controllers","l":"Login"},{"p":"curso.java.tienda.controllers","l":"UsuarioGest"}];updateSearchResults();
+typeSearchIndex = [{"p":"curso.java.tienda.Filters","l":"AccessFilter"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"curso.java.tienda.controllers","l":"Login"}];updateSearchResults();
